5655fb96f340fb958ee54da9d5fe55ba22e643ee
Kazi Abdullah Al Mamun <mamun@siliconorchard.com>
2018-10-02 09:01:15 +0600

    Initial Commit

---
+++ PracticeCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeCoreData
+//
+//  Created by Mr. Hasan on 10/2/18.
+//  Copyright © 2018 slickdevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
